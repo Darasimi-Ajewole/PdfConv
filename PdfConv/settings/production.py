e0137a4c324561d3839017baab20ced8e7c188a7
Darasimi Ajewole <darasimiajewole@gmail.com>
2019-07-10 08:10:54 +0100

    specify  django log level

--- PdfConv/settings/production.py
+++ PdfConv/settings/production.py
@@ -10,6 +10,16 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s for s in v.split(',')]
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
