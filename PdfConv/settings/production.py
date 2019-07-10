829ef08f72ee7cd682a0841e3b51e2d5084369f2
Darasimi Ajewole <darasimiajewole@gmail.com>
2019-07-10 07:48:57 +0100

    including logs for server error

--- PdfConv/settings/production.py
+++ PdfConv/settings/production.py
@@ -6,3 +6,20 @@ DEBUG = config('DEBUG',cast=bool)
 SECRET_KEY = config('SECRET_KEY')
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s for s in v.split(',')])
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
