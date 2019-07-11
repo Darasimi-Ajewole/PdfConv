8328f4a53c2faeb0388a9de451486372341d1059
Darasimi Ajewole <darasimiajewole@gmail.com>
2019-07-11 19:19:24 +0100

    debugging

--- main/tasks.py
+++ main/tasks.py
@@ -7,7 +7,7 @@ import os
 @shared_task
 def convert(file_path):
     print(2222222233,os.listdir(settings.MEDIA_ROOT))
-
+    print('hope',file_path)
     process = Popen(["abiword","--to=pdf",file_path],stdout=PIPE)
     process.communicate()
     print(2222222233,os.listdir(settings.MEDIA_ROOT))
