0f2b73e0a489eb3f7feab1aa15e8fbba586e6c37
Darasimi Ajewole <darasimiajewole@gmail.com>
2019-07-13 00:07:16 +0100

    Production fix

--- main/tasks.py
+++ main/tasks.py
@@ -8,7 +8,7 @@ import os
 def convert(file_path):
     print(2222222233,os.listdir(settings.MEDIA_ROOT))
     print('hope',file_path)
-    process = Popen(["UNO_PATH=/app/.apt/usr/lib/libreoffice","unoconv","-f","pdf",file_path],stdout=PIPE)
+    process = Popen(["/app/.apt/usr/lib/libreoffice","unoconv","-f","pdf",file_path],stdout=PIPE)
     #process = Popen(["abiword","--to=pdf",file_path],stdout=PIPE)
     process.communicate()
     print(2222222233,os.listdir(settings.MEDIA_ROOT))
