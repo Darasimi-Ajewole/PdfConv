0623dc4116cefa1854c3efb02a9d59d3c9a2e744
Darasimi Ajewole <darasimiajewole@gmail.com>
2019-07-13 01:22:50 +0100

    production things

--- main/tasks.py
+++ main/tasks.py
@@ -8,7 +8,7 @@ import os
 def convert(file_path):
     print(77777777733,os.listdir(settings.MEDIA_ROOT))
     print('hope',file_path)
-    process = Popen(["/app/.apt/usr/lib/python3","unoconv","-f","pdf",file_path],stdout=PIPE)
+    process = Popen(["unoconv","-f","pdf",file_path],stdout=PIPE)
     #process = Popen(["abiword","--to=pdf",file_path],stdout=PIPE)
     process.communicate()
     print(2222222233,os.listdir(settings.MEDIA_ROOT))
