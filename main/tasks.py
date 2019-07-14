dc0b6ba68dd8b556d83c70a301cd3cff3609c899
Darasimi Ajewole <darasimiajewole@gmail.com>
2019-07-14 03:49:12 +0100

    intermediate introduced

--- main/tasks.py
+++ main/tasks.py
@@ -7,9 +7,11 @@ import os
 @shared_task
 def convert(file_path):
     print(77777777733,os.listdir(settings.MEDIA_ROOT))
-    print('hope',file_path)
-    process = Popen(["unoconv","-f","pdf",file_path],stdout=PIPE)
-    #process = Popen(["abiword","--to=pdf",file_path],stdout=PIPE)
+    print('hope',file_path)   
+    process = Popen(["docx2txt",file_path],stdout=PIPE)
+    #process = Popen(["unoconv","-f","pdf",file_path],stdout=PIPE)
+    process.communicate()
+    process = Popen(["abiword","--to=pdf",file_path.replace('docx','txt')],stdout=PIPE)
     process.communicate()
     print(2222222233,os.listdir(settings.MEDIA_ROOT))
 
