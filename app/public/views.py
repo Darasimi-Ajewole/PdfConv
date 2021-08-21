570ccea58680c068878d8dc6d59d2e7c509524db
Darasimi Ajewole <darasimi@formpl.us>
2021-08-21 02:22:12 +0100

    Broke util into sub modules

--- app/public/views.py
+++ app/public/views.py
@@ -4,12 +4,15 @@ from . import public_api
 from .schema import (GenerateUploadSessionModel,
                      GenerateUploadSessionResponse)
 from flask_restx import Resource, abort
-from utils import generate_upload_url, check_upload
+from utils.cloudstorage import generate_upload_url, check_upload
+from utils.converter import start_session
+from utils import taskqueue
 
 
 @public_api.route('/hello')
 class HelloWorld(Resource):
     def get(self):
+        taskqueue.create_dummy_task()
         return {'hello': 'world'}
 
 
@@ -26,15 +29,11 @@ class GenerateUploadSession(Resource):
         return {'upload_url': upload_url, 'blob_name': blob_name}
 
 
-@public_api.route('/validate-upload/<path:blob_name>')
-class ValidateUpload(Resource):
-    def get(self, blob_name):
+@public_api.route('/convert-document/<path:blob_name>')
+class ConvertDocument(Resource):
+    def post(self, blob_name):
         valid_upload = check_upload(blob_name)
         if not valid_upload:
             return abort(404, 'Upload was not completed')
-        return '', 200
-
-
-class ConvertDocument(Resource):
-    def post(self):
-        pass
+        task = start_session(blob_name)
+        return f'Conversion Task -{task.name}, Enqueued Successfully', 200
