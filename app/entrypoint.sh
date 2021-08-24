9e11837f6a78931690e32b6370092369df69c891
Darasimi Ajewole <darasimi@formpl.us>
2021-08-24 06:26:21 +0100

    Remove unneeded files

--- app/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $DEBUG == 1 ]]; then
-echo "Setting GOOGLE_APPLICATION_CREDENTIALS"
-export GOOGLE_APPLICATION_CREDENTIALS="service_account/open-source-320820-b7123898a659.json"
-fi
-
-
-python3 main.py
--- main/forms.py
+++
@@ -1,7 +0,0 @@
-from main.models import Task
-from django.forms import ModelForm
-
-class TaskForm(ModelForm):
-    class Meta:
-        model = Task
-        fields = ["doc"]
