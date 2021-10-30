200a48ec032a0345a53907d85f52637f094b9a71
Darasimi Ajewole <darasimi@formpl.us>
2021-10-30 08:13:31 +0100

    fixed broken syntax

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 deploy_cloud_run(){
   cd app
-  gcloud builds submit --tag gcr.io/$GCP_PROJECT/pdf-conv:latest &&
+  # gcloud builds submit --tag gcr.io/$GCP_PROJECT/pdf-conv:latest
 
 }
 
@@ -12,11 +12,6 @@ deploy_asset(){
   yarn build && 
   cp -r build ../firebase
 }
-
-
-trap terminate SIGINT EXIT
-
-echo 'Deploying Project....' 
 
 deploy_cloud_run &
 deploy_asset &
