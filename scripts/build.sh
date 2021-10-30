292f1fe2280d014de154221ef4b60da429f728e2
Darasimi Ajewole <darasimi@formpl.us>
2021-10-30 12:24:28 +0100

    Fixing missing service key

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,10 @@
 
 build_app(){
   cd app
-  gcloud builds submit --tag gcr.io/$GCP_PROJECT/pdf-conv:latest
+  echo 'Creating service account key'
   mkdir -p service_account
   echo $FIREBASE_KEY >> ./service_account/pdf-conv-3d1f5-firebase-adminsdk-iiadp-232350456b.json
+  gcloud builds submit --tag gcr.io/$GCP_PROJECT/pdf-conv:latest
 }
 
 build_asset(){
@@ -28,3 +29,5 @@ case $1 in
         echo "Invalid arg $1"
         exit 1 ;;
 esac
+
+cd ..
