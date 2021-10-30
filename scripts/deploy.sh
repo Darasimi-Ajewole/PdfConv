565ba60838458e8acc5e0c0d9c4e6aa0d5c5a14b
Darasimi Ajewole <darasimi@formpl.us>
2021-10-30 08:11:45 +0100

    Fixed broken firebase deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,18 +3,14 @@
 deploy_cloud_run(){
   cd app
   gcloud builds submit --tag gcr.io/$GCP_PROJECT/pdf-conv:latest &&
-  gcloud run deploy default \
-    --image gcr.io/$GCP_PROJECT/pdf-conv:latest \
-    --region=us-central1 \
-    --service-account $CLOUD_RUN_ACCOUNT \
-    --allow-unauthenticated \
 
 }
 
 deploy_asset(){
   cd assets
   yarn install &&
-  yarn build
+  yarn build && 
+  cp -r build ../firebase
 }
 
 
